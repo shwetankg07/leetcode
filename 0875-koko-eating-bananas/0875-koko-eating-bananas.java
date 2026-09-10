971576dcf998fc072cc4d4d3e238770f59a6224a
shwetank gupta <shwetankg07@gmail.com>
2026-09-10 14:28:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
+        int low = 1;
+        int high = 0;
+        int ans = 0;
+        for (int i : piles) {
+            if (i > high) {
+                high = i;
+            }
+        }
+        while (high >= low) {
+            int mid = low + (high - low) / 2;
+            if (candoit(piles, h, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public boolean candoit(int[] A, int B, int k) {
+        int totalhours = 0;
+        for (int i : A) {
+            totalhours += (i + k - 1) / k;
+        }
+        return totalhours <= B;
+    }
+}
