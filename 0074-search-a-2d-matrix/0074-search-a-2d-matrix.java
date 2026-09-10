bf3bb1e0a498076aaefc29455571fc9336daf4cb
shwetank gupta <shwetankg07@gmail.com>
2026-09-10 21:08:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,28 +2,21 @@ class Solution {
     public boolean searchMatrix(int[][] A, int B) {
        int n = A.length;
         int m = A[0].length;
-        int z = n * m;
-        int kick[] = new int[z];
-        int k = 0;
-        for (int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                kick[k++] = A[i][j];
-            }
-        }
         int l = 0;
-        int r = z-1;
-        while(r >= l){
-            int mid = l + (r-l)/2;
-            if(kick[mid] < B){
-                l = mid + 1;
+        int r = n * m - 1;
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            int element = A[mid/m][mid%m];
+            if(element == B){
+                return true;
             }
-            else if(kick[mid] > B){
+            else if(element > B){
                 r = mid - 1;
             }
             else{
-                return true;
+                l = mid + 1;
             }
         }
-        return false; 
+        return false;
     }
 }
